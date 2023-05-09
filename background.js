923e5cf7b93e7483a6891073ffabc56ede2c7a6e
hdc <nomad.hwang@gmail.com>
2023-05-09 11:24:21 +0900

    chore: 필요 없는 부분 정리

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ async function reddenPage() {
     const articles = Array.from(document.querySelector('#cafe_main').contentWindow.document.querySelectorAll('.type_up'));
 
     if (articles.length === 0) {
-        alert('좋아요가 없어 형');
+        alert('추천글이 없어 형');
         return;
     }
     
@@ -12,14 +12,11 @@ async function reddenPage() {
     for (article of articles) {
         const link = article.querySelector('a.article');
         const articleId = link.href.split('articleid=')[1];        
-        
 
         link.setAttribute('target', '_blank');
         link.click();
 
         articlesToRemove.push(articleId);
-
-        console.log("Done: " + articleId);
     }
     
     console.log("Open Done");
@@ -31,12 +28,7 @@ async function reddenPage() {
     for (articleId of articlesToRemove) {
         // 좋아요 목록에서 해당 게시글 삭제
         // HideUpArticle.nhn?clubid=30893734&page=1&menuid=3&boardtype=L&articleid=' + articleId;
-        await fetch('https://cafe.naver.com/HideUpArticle.nhn?clubid=' + clubId + '&page=1&menuid=' + menuId + '&boardtype=L&articleid=' + articleId, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
+        await fetch('https://cafe.naver.com/HideUpArticle.nhn?clubid=' + clubId + '&page=1&menuid=' + menuId + '&boardtype=L&articleid=' + articleId);
     }
 
     console.log("Remove Done. Refreshing...");
